2c9935e8dbdbd16c0fbaceb4d3fbaa03db6c1148
Tony <chiangloveslynn@gmail.com>
2016-05-14 15:00:01 +0800

    some demo

---
+++ app/scripts/custom-mod.js
@@ -0,0 +1,11 @@
+// module.exports.speak = () => console.log('hello world');
+module.exports = {
+    doA: function () {
+        'use strict';
+        console.log('doA');
+    },
+    doB: function () {
+        'use strict';
+        console.log('doB');
+    }
+};
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,11 @@
 //Browserify
+//Require
 const $ = require('./jquery.min');
+const custom = require('./custom-mod');
+const customb = require('./custom-modB');
 
+//Do Something.
 $('body').append('<h1>Jquery Required.</h1>');
+
+custom.doA();
+customb();
